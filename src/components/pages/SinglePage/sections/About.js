d47a566a53af1ee9a2c02c12049d383fb7aebf97
Matthew Cromer <matthew.h.cromer@gmail.com>
2021-10-31 18:22:27 -0700

    tweak about section

--- src/components/pages/SinglePage/sections/About.js
+++ src/components/pages/SinglePage/sections/About.js
@@ -11,18 +11,18 @@ export default function About() {
         <ContentTitle>About</ContentTitle>
         <P>
           Have you ever seen those car commercials where there's a bunch of <b>robots</b>{' '}
-          welding panels onto cars? My early career was spent designing things like that.
+          welding panels onto cars? I started my career designing things like that.
         </P>
         <P>
-          Not long after, I got involved with a small business and used those skills to{' '}
-          <b>transform</b> it from a heat exchanger <i>distributor</i> into a heat exchanger{' '}
+          Not long after, I got involved with a small business and used the skills I'd gained
+          to <b>transform</b> it from a heat exchanger <i>distributor</i> into a heat exchanger{' '}
           <i>manufacturer</i>.
         </P>
         <P>
-          Because it was a small business, I wore many hats. This was when software became a
-          medium for improving the business and I was finally{' '}
-          <b>awakened to my passion for software</b>. I began teaching myself to code and
-          reading books about software (I highly recommend{' '}
+          As is often required in high growth companies, I wore many hats. This was when
+          software became a medium for improving the business and I was finally{' '}
+          <b>awakened to my passion</b>. I began teaching myself to code and reading books
+          about software (I highly recommend{' '}
           <A
             href='https://www.amazon.com/Life-3-0-Being-Artificial-Intelligence/dp/0141981806/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1631851323&sr=8-1'
             target='_blank'>
@@ -31,10 +31,10 @@ export default function About() {
           !)
         </P>
         <P>
-          I'm extremely proud of what I've learned and accomplished since my epiphany. I've
-          integrated <b>information systems</b> on Ford assembly lines, built an MRP{' '}
-          <b>web application</b>, and currently serve an incredibly talented software team as{' '}
-          <b>product owner</b>.
+          I'm extremely proud of what I've learned and accomplished since I dove into the world
+          of software. I've integrated <b>information systems</b> on Ford assembly lines, built
+          an MRP <b>web application</b>, and currently serve an incredibly talented software
+          team as <b>product owner</b>.
         </P>
         <P>I'm so excited for what the future holds. Onwards.</P>
       </Content>
